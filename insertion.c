5bfe7a8fed5ace612c48afdcd562df4b85e798b8
FARHANA3491 <93931465+FARHANA3491@users.noreply.github.com>
2021-12-01 17:01:25 +0530

    Create insertion.c

---
+++ insertion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main()
+{
+  struct node{
+ node;
+    else
+        newnode->next=head;
+        head=newnode;        int data;
+      struct node *next;
+  }
+}
+
+void insertbeg(int num)
+{
+    struct node *newnode;
+    newnode = (struct node*) malloc (size of (struct node));
+    newnode->data=num;
+    newnode->next=null;
+    if(head==null)
+        head= newnode;
+}
+ void insertend(int num)
